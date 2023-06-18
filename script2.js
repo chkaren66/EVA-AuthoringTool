867cf0fac879c58ea59d09b81f3e009698091508
Serboda <sebasm.sw@gmail.com>
2023-06-18 03:07:57 -0400

    Swiper slides without content

--- script2.js
+++ script2.js
@@ -1,19 +1,21 @@
-var swiper = new Swiper(".slide-content", {
-  slidesPerView: 1,
-  spaceBetween: 25,
-  loop: false,
-  centerSlide: 'true',
-  fade: 'true',
-  grabCursor: 'true',
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-    dynamicBullets: true,
-  },
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
-
- 
-});
+const largeSlider = ()=>{
+	let largeSliders = document.querySelectorAll('.large-swiper')
+	let prevArrow = document.querySelectorAll('.prev')
+	let nextArrow = document.querySelectorAll('.next')
+	largeSliders.forEach((slider, index)=>{
+    // this bit checks if there's more than 1 slide, if there's only 1 it won't loop
+		let sliderLength = slider.children[0].children.length
+		let result = (sliderLength > 1) ? true : false
+		const swiper = new Swiper(slider, {
+			direction: 'horizontal',
+			loop: false,
+			navigation: {
+        // the 'index' bit below is just the order of the class in the queryselectorAll array, so the first one would be NextArrow[0] etc
+				nextEl: nextArrow[index],
+				prevEl: prevArrow[index],
+			},
+			speed: 1000,
+		});	
+	})
+}
+window.addEventListener('load', largeSlider)
